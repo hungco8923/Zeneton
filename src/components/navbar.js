dea19885fba49e26d2a5dbb2a985e9960dc4087d
hungco8923 <hungco8923@gmail.com>
2023-11-06 14:55:41 +0700

    fix(navbar): fix navbar overload. Refs #ZEN-5

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,12 +11,8 @@ window.onscroll = function() {
 
   if (prevScrollPos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    console.log("Prev: " + prevScrollPos);
-    console.log("Curr: " + currentScrollPos);
   } else {
-    document.getElementById("navbar").style.top = "-50px";
-    console.log("Prev: " + prevScrollPos);
-    console.log("Curr: " + currentScrollPos);
+    document.getElementById("navbar").style.top = "-78px";
   }
   prevScrollPos = currentScrollPos;
 };
